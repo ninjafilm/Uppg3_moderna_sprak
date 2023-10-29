0e557eb60a61a9b4cb25fdce1be7cc074fee2cdf
Michael Åkerman <michael@ninjafilm.com>
2023-10-29 20:17:04 +0100

    Ändrat text

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hej $name!")
 }
 
 @Preview(showBackground = true)
